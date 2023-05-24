622aee97cbdc0a96c92319159e4b2eb0ac2eaf64
Karen Byrd <klbyrd88@yahoo.com>
2023-05-23 22:05:13 -0500

    created calendar and title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import {useState} from 'react';
+import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./images/icons8_bee.svg" alt="Honey Beez Logo" class="bee" />
-        <h1 id="title">Honey Beez Todo App</h1>
-      </header>
+    <div className="app">
+      <h1 className='title'>Honey Beez</h1>
+      <div className="calendar-container">
+        <Calendar onChange = {setDate} value={date}/>
+      </div>
+      <div className="text-center">
+        Selected date: {date.toDateString()}
+      </div>
     </div>
   );
 }
